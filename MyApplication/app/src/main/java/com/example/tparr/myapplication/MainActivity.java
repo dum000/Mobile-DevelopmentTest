3e9c1dd5ddec9c4953facfd3f7eb73a8d2e03d73
dum000 <timmy_parr@yahoo.com>
2017-02-21 12:30:47 -0500

    Added Example application with Search Capability.

--- MyApplication/app/src/main/java/com/example/tparr/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/tparr/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.tparr.myapplication;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 }
