5ba5167b93718089894ab26a5a8f4788a14dd194
dum000 <Timmy_parr@yahoo.com>
2017-05-07 15:21:57 -0400

    Added Database commands for Searches

--- ApplicationFundamentals/app/src/main/java/parrtim/applicationfundamentals/DictionaryOpenHelper.java
+++ ApplicationFundamentals/app/src/main/java/parrtim/applicationfundamentals/DictionaryOpenHelper.java
@@ -8,16 +8,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DictionaryOpenHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 2;
-    private static final String DICTIONARY_TABLE_NAME = "Draft";
-    private static final String NUMBER_COLUMN = "Number";
-    private static final String MESSAGE_COLUMN = "ReceivedMessage";
-    private static final String DRAFT_COLUMN = "DraftMessage";
     private static final String DATABASE_NAME = "ApplicationFundamentals";
-    private static final String DICTIONARY_TABLE_CREATE =
-            "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
-                    NUMBER_COLUMN + " TEXT, " +
-                    MESSAGE_COLUMN + " TEXT, " +
+
+    //DRAFT TABLE
+    private static final String DRAFT_TABLE_NAME = "Draft";
+    private static final String DRAFT_NUMBER_COLUMN = "Number";
+    private static final String DRAFT_MESSAGE_COLUMN = "ReceivedMessage";
+    private static final String DRAFT_COLUMN = "DraftMessage";
+    private static final String DRAFT_TABLE_CREATE =
+            "CREATE TABLE " + DRAFT_TABLE_NAME + " (" +
+                    DRAFT_NUMBER_COLUMN + " TEXT, " +
+                    DRAFT_MESSAGE_COLUMN + " TEXT, " +
                     DRAFT_COLUMN + " TEXT);";
+
+    //SEARCH TABLE
+    private static final String SEARCH_TABLE_NAME = "Search";
+    private static final String SEARCH_QUERY_COLUMN = "Query";
+    private static final String SEARCH_DATE_COLUMN = "DATE";
+    private static final String SEARCH_TABLE_CREATE =
+            "CREATE TABLE " + SEARCH_TABLE_NAME + " (" +
+                    SEARCH_QUERY_COLUMN + " TEXT, " +
+                    SEARCH_DATE_COLUMN + " DATE);";
 
     private SQLiteDatabase database;
 
@@ -25,8 +36,11 @@ public class DictionaryOpenHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         try {
             database = context.openOrCreateDatabase(DATABASE_NAME, 0, null);
-            if (!isTableExists(DICTIONARY_TABLE_NAME, true)) {
-                database.execSQL(DICTIONARY_TABLE_CREATE);
+            if (!isTableExists(DRAFT_TABLE_NAME, true)) {
+                database.execSQL(DRAFT_TABLE_CREATE);
+            }
+            if (!isTableExists(SEARCH_TABLE_NAME, true)) {
+                database.execSQL(SEARCH_TABLE_CREATE);
             }
         } catch (Exception e1) {
             throw e1;
@@ -35,12 +49,14 @@ public class DictionaryOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(DICTIONARY_TABLE_CREATE);
+        db.execSQL(DRAFT_TABLE_CREATE);
+        db.execSQL(SEARCH_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        database.execSQL("DROP TABLE IF EXISTS " + DICTIONARY_TABLE_NAME);
+        database.execSQL("DROP TABLE IF EXISTS " + DRAFT_TABLE_NAME);
+        database.execSQL("DROP TABLE IF EXISTS " + SEARCH_TABLE_NAME);
         onCreate(database);
     }
 
@@ -56,7 +72,7 @@ public class DictionaryOpenHelper extends SQLiteOpenHelper {
             }
         }
 
-        Cursor cursor = database.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '"+tableName+"'", null);
+        Cursor cursor = database.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '" + tableName + "'", null);
         if(cursor!=null) {
             if(cursor.getCount()>0) {
                 cursor.close();
